5b49df3fa2f2e17fb75abec4fe0da65cce46ad48
biulove0x <105073212+biulove0x@users.noreply.github.com>
2022-06-26 19:16:51 +0700

    Update domain_to_ip.sh

--- domain_to_ip.sh
+++ domain_to_ip.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 # @author : biulove0x
 # @name   : Convert Domain to IP Address
+banner()
+{
+    echo "##########################################"
+    echo "# @author : biulove0x                    #"
+    echo "# @name   : Convert Domain to IP Address #"
+    echo "##########################################"
+}
+banner
+for domain in $(cat $1);
+do
+    # Get IP Address
+    ip=$(dig +short $domain)
+    # Output
+    echo "${domain} => ${ip}" | tr '\n' ' '
+    echo
+    # Save
+    echo "${ip}" >> results.txt
+done
